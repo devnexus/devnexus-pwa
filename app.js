be6e3131b8f1bb6278f3a63182d86e826c09a51f
Summers Pittman <secondsun@gmail.com>
2018-02-04 10:52:55 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+var cacheName = 'shell-content';
+var filesToCache = [
+  '/css/styles.css',
+  '/js/scripts.js',
+  '/images/logo.svg',
+
+  '/offline.html’,
+
+  '/’,
+];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});

d33f37c2960e8a9cc40cf32b7bd48649fa3c04de
Summers Pittman <secondsun@gmail.com>
2018-02-17 18:45:46 -0500

    Details

--- script/ScheduleDetail.js
+++ script/ScheduleDetail.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Typography from 'material-ui/Typography';
 import PropTypes from 'prop-types';
 import Button from 'material-ui/Button';
 import Dialog, {
@@ -45,55 +46,49 @@ export class ScheduleDetail extends React.Component {
           fullScreen={fullScreen}
           open={this.state.open}
           onClose={this.handleClose}
-          aria-labelledby="responsive-dialog-title"
         >
-          <DialogContent>
+          
           <div id="speaker" className="bg-light-gray details container"
-              style={{marginTop: "2em", textAlign: "left", paddingBottom: 0}}>
+              style={{textAlign: "left", paddingBottom: 0}}>
               <div className="row" key={this.state.item.id}>
                 <div className="col-sm-10 col-sm-offset-1">
                   <div className="speaker-member row "
                     style={{padding:"25px",
                     background: "linear-gradient(-90deg, #6c7070, #6c7070) repeat-y", backgroundSize: "10px 10px",
                     backgroundColor: "#282828"}}>
-                    {this.state.item.title}
-                      <div>
-                        Track:
-                        { this.state.item.track }
+                    <Typography style={{color: "#fff",fontSize: "26px",fontWeight: "700"}}>
+                      {this.state.item.title}
+                    </Typography>
+                      <div style={{backgroundColor:"#282828"}}>
+                          <Typography style={{color: "#fff",fontWeight: "700", display:"inline"}}>
+                            Track:
+                          </Typography>
+                          <Typography style={{color: "#fff", display:"inline"}}>
+                            { this.state.item.track }
+                          </Typography>
                       </div>
                   </div>
               </div>
               </div>
               <div className="row" key={this.state.item.id + "1"}>
                 <div className="col-sm-10 col-sm-offset-1 ">
-                  <div className="presentation-header" key={this.state.item.id + "2"}>
+                  <Typography className="presentation-header" >
                     Abstract
-                  </div>
-                  <div className="biographyBody" key={this.state.item.id + "3"}>
+                  </Typography>
+                  <Typography className="biography-body" >
                     {this.state.item.abstract}
-                  </div>
+                  </Typography>
                 </div>
               </div>
               {this.state.item.persons?this.state.item.persons.map((speaker, i) => {
                 return (<div key={this.state.item.id + "2" + i + "3"}>
-                          <div key={this.state.item.id + "1" + i + "1"}>{speaker.full_public_name}</div>
-                          <div key={this.state.item.id + "1" + i + "2"}>{speaker.abstract}</div>
+                          <Typography className="biography-header" >{speaker.full_public_name}</Typography>
+                          <Typography className="biography-body" >{speaker.abstract}</Typography>
                 </div>);
               }):<div key={this.state.item.id + "1" + "1"}/> }
-                {/* {% for speaker in page.persons %}
-                  {% include speaker_bio.html speaker=speaker %}
-                {% endfor %} */}
-
             </div>  
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={this.handleClose} color="primary">
-              Disagree
-            </Button>
-            <Button onClick={this.handleClose} color="primary" autoFocus>
-              Agree
-            </Button>
-          </DialogActions>
+          
+
         </Dialog>
       </div>
     );
